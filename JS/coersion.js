70c062060c63fb7b129d2148335fc2c53cfacf44
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-05-14 04:23:09 +0530

    COERSION

---
+++ JS/coersion.js
@@ -0,0 +1,25 @@
+/*
+Coersion is type inter-conversion
+
+In cpp types exists for the variables
+
+In JS types exists for the values
+
+Everythis in JS is an object-> False
+
+Abstract operations are not a part of ECMA Script, they are just here to aid the semantics of the script.
+Read documentation of abstract operations.
+*/
+
+/**
+ * if primitive is number: valueOf() then toString()
+ * if primitibe is string: toString() then valueOf()
+ */
+
+console.log(10-{"a":10});
+console.log(10-{"a":10, valueOf(){ return 5;}});
+console.log(10-{"a":10,valueOf(){return {};}});
+console.log(10-{"a":10,valueOf(){return {};},toString(){return "5";}});// if value of does not return primitive then toString is called.
+console.log(10-{"a":10, valueOf(){ return "2";}});
+
+console.log(12+{"x":23});//first valueOf the toString
--- JS/variables.js
+++ JS/variables.js
@@ -5,3 +5,5 @@ console.log(x,y,z);
 console.log(-4||5)// if the first value is true, return the first value in case of or
 console.log(3&&4);// if the condition is true return the second value in case of and
 console.log(3||4);
+
+console.log("5">>1);// coercion
